cc944dd667b1e3647dcad8406263f1332805a289
xuoutput <847438919@qq.com>
2023-04-06 22:50:23 +0800

    feat(rss): add rss

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,11 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+export async function get() {
+  return rss({
+    title: 'Astro Learner | Blog',
+    description: 'My journey learning Astro',
+    site: 'https://roaring-truffle-c34c27.netlify.app/',
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>en-us</language>`,
+  });
+}
